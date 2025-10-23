d28e6c74180aaa6f48fac5f8a9a9ad3556e883ee
Erick-Faedda <erickfaedda15@gmail.com>
2025-10-23 09:56:01 +0100

    Func Soma
    Funçao soma

--- soma.c
+++ soma.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+
+int soma(int c, int e);
+
 int main(){
     int c, e;
 
     printf("Insira dois numeros: ");
     scanf("%d" "%d", &c, &e);
+
+    printf("%d", soma(c, e));
     return 0;
+}
+
+
+int soma(int c, int e){
+    return c + e;
+    
 }
